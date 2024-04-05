305352272ad06caaf8f753e67c4c4acc55884b14
Hubert Frączkowski <hubert.fraczkowski@primopartner.pl>
2024-04-05 17:21:11 +0200

    simple change in project

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Tu można dodać kod JS, np. do obsługi formularza kontaktowego
 });
+
+document.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var imageSection = document.getElementById('image-section');
+    imageSection.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
